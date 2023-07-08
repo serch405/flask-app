1ff6ecf4bc594f42c199dd7e9169083c81a60b9a
serch405 <cherevychenkosa@gmail.com>
2023-07-08 16:22:39 +0200

    Write a test for users endpoint

--- tests/test_pages.py
+++
@@ -1,9 +0,0 @@
-def test_pass(app, client):
-    res = client.get('/')
-    assert res.status_code == 200
-    assert "Register or login to get inside" in res.get_data(as_text=True)
-    
-def test_fail(app, client):
-    res = client.get('/')
-    assert res.status_code == 404
-    
