9cb02dac9b73dfd7b7a64d30a512a669a1571e25
serch405 <cherevychenkosa@gmail.com>
2023-07-08 16:13:51 +0200

    Add register end point

--- api.py
+++ api.py
@@ -1,9 +1,15 @@
 from flask import Blueprint, request, abort, jsonify
-from config import app, mail
+from config import app, db, mail
 from models import User
+from werkzeug.security import generate_password_hash
 
 
 api_bp = Blueprint('api', __name__, url_prefix='/api')
+
+@app.errorhandler(400)
+def custom400(error):
+    response = jsonify({'message': error.description['message']})
+    return response, 400
 
 @api_bp.route('/user')
 def get_user():
@@ -34,3 +40,24 @@ def get_users():
         return jsonify(users=user_list)
     else:
         abort(404)
+
+@api_bp.route('/register', methods=['POST'])
+def register():
+    username = request.args.get('username')
+    email = request.args.get('email')
+    password = request.args.get('password')
+    repass = request.args.get('repass')
+        
+    if password != repass:
+        abort(400, {'message': 'Passwords don\'t match'})
+            
+    user_found_by_username = User.query.filter_by(username=username).first()
+    user_found_by_email = User.query.filter_by(email=email).first()
+    if user_found_by_username or user_found_by_email:
+        abort(400, {'message': 'Username or Email already exists'})
+
+    new_user = User(username=username, email=email, password=generate_password_hash(password))
+    db.session.add(new_user)
+    db.session.commit()
+
+    return "Success", 200
